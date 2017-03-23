d987f5e1397878852b8ef9018b36972085ddf625
Jesus Antonio JD <juarez_devora_ja@hotmail.com>
2017-03-23 08:29:24 -0800

    Add files via upload

---
+++ ConexionFinal/ConexionFinal/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ConexionFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Dgview = new System.Windows.Forms.DataGridView();
+            this.consulta = new System.Windows.Forms.Button();
+            this.insert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.btnconex = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.txtnombres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnordenar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dgview
+            // 
+            this.Dgview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgview.Location = new System.Drawing.Point(1, -2);
+            this.Dgview.Name = "Dgview";
+            this.Dgview.Size = new System.Drawing.Size(299, 347);
+            this.Dgview.TabIndex = 0;
+            // 
+            // consulta
+            // 
+            this.consulta.Location = new System.Drawing.Point(329, 12);
+            this.consulta.Name = "consulta";
+            this.consulta.Size = new System.Drawing.Size(75, 23);
+            this.consulta.TabIndex = 1;
+            this.consulta.Text = "CONSULTA";
+            this.consulta.UseVisualStyleBackColor = true;
+            this.consulta.Click += new System.EventHandler(this.consulta_Click);
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(329, 231);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 23);
+            this.insert.TabIndex = 2;
+            this.insert.Text = "INSERTAR";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NOMBRE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EDAD";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(317, 273);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 5;
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(317, 312);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(100, 20);
+            this.txtedad.TabIndex = 6;
+            // 
+            // btnconex
+            // 
+            this.btnconex.Location = new System.Drawing.Point(329, 186);
+            this.btnconex.Name = "btnconex";
+            this.btnconex.Size = new System.Drawing.Size(75, 23);
+            this.btnconex.TabIndex = 7;
+            this.btnconex.Text = "CONEXION";
+            this.btnconex.UseVisualStyleBackColor = true;
+            this.btnconex.Click += new System.EventHandler(this.btnconex_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(329, 105);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 8;
+            this.btneliminar.Text = "ELIMINAR";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // txtnombres
+            // 
+            this.txtnombres.Location = new System.Drawing.Point(317, 151);
+            this.txtnombres.Name = "txtnombres";
+            this.txtnombres.Size = new System.Drawing.Size(100, 20);
+            this.txtnombres.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "NOMBRE";
+            // 
+            // btnordenar
+            // 
+            this.btnordenar.Location = new System.Drawing.Point(329, 59);
+            this.btnordenar.Name = "btnordenar";
+            this.btnordenar.Size = new System.Drawing.Size(75, 23);
+            this.btnordenar.TabIndex = 11;
+            this.btnordenar.Text = "ORDENAR";
+            this.btnordenar.UseVisualStyleBackColor = true;
+            this.btnordenar.Click += new System.EventHandler(this.btnordenar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Alumno: Jesus Antonio Juarez Devora";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 338);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnordenar);
+            this.Controls.Add(this.txtnombres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnconex);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.consulta);
+            this.Controls.Add(this.Dgview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MYSQL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Dgview;
+        private System.Windows.Forms.Button consulta;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Button btnconex;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox txtnombres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnordenar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ConexionFinal/ConexionFinal/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ConexionFinal
+{
+    public partial class Form1 : Form
+    {
+        //se construye la direccion de la bd
+        MySqlConnectionStringBuilder direcc = new MySqlConnectionStringBuilder();
+        //Se crea una conexion a mysql
+        MySqlConnection conexion = null;
+        MySqlCommand comando = null;
+        MySqlDataAdapter con = null;
+        DataSet ds = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            direcc.Server = "localhost";//localhost o 127.0.0.1
+            direcc.UserID = "root";
+            direcc.Password = "hola";
+            direcc.Database = "eventos";
+            //Guardamos el constructor de la coneccion (convertido a string), en la coneccion
+            conexion = new MySqlConnection(direcc.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conex();
+        }
+
+        private void conex()
+        { 
+            //Se intenta conexion
+            try
+            {
+                conexion.Open();
+                //Muestra el mensaje
+                MessageBox.Show("La coneccion se realizo correctamente", "Conexion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conexion.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al conectarse", "Conexion", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                conexion.Close();
+            }
+
+         }
+
+        private void btnconex_Click(object sender, EventArgs e)
+        {
+            conex();
+        }
+        
+        private void consult()
+        {
+            try
+            {            
+                conexion.Open();
+                //aqui le decimos que le vamos a pasar un comando '" + txtnombres.Text + "'
+                comando = new MySqlCommand("Select * from Usuarios ", conexion);
+                //Creamos un adaptador de informacion
+                //Recupera la totalidad de la tabla
+                con = new MySqlDataAdapter(comando);
+                //Para los valores que obtengamos de la tabla
+                //Representa un conjunto de datos, tablas,
+                ds = new DataSet();
+                //Se llenan los valores y los guarda el adaptador
+                //utiliza el formato de la tabla
+                con.Fill(ds);
+                //Se muestra en el DataGridView, se empieza en la posicion 
+                //Para el origen de los datos
+                Dgview.DataSource = ds.Tables[0];
+                //Cerramos la conexion
+                conexion.Close();
+            }
+            catch (MySqlException error)
+            {
+                MessageBox.Show("Error al consultar " + error);
+                conexion.Close();
+            }
+        }
+
+        private void consulta_Click(object sender, EventArgs e)
+        {
+            consult();   
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                comando = new MySqlCommand("insert into Usuarios(nombre,edad)values('"+txtnombre.Text+"','"+txtedad.Text+"')", conexion);
+                txtnombre.Text = ""; txtedad.Text = "";
+                //Ejecuta la instruccion del comando y devuelve el numero de columnas afectada
+                int uno = comando.ExecuteNonQuery();
+                MessageBox.Show("Numero de filas afectada: " + uno,"Informacion",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                conexion.Close();
+                consult();
+            }
+            catch (MySqlException errorr)
+            {
+                MessageBox.Show("Error al insertar " + errorr);
+                conexion.Close();
+            }
+
+            }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                comando = new MySqlCommand("delete from Usuarios where nombre='" + txtnombres.Text + "'", conexion);
+                txtnombres.Text = "";
+                //Ejecuta el comando de arriba y nos dice cuantas filas se afectaron
+                int uno = comando.ExecuteNonQuery();
+                MessageBox.Show("Numero de filas afectada: " + uno, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conexion.Close();
+                consult();
+            }
+            catch (MySqlException errorr)
+            {
+                MessageBox.Show("Error al insertar " + errorr);
+                conexion.Close();
+            }
+    }
+        
+        private void btnordenar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                comando = new MySqlCommand("Select * from Usuarios order by nombre", conexion);
+                con = new MySqlDataAdapter(comando);
+                //Para los valores que obtengamos de la tabla
+                ds = new DataSet();
+                //Se llenan los valores y los guarda el adaptador
+                con.Fill(ds);
+                //Se muestra en el DataGridView, se empieza en la posicion 
+                Dgview.DataSource = ds.Tables[0];
+                //Cerramos la conexion
+                conexion.Close();
+            }
+            catch (MySqlException errorr)
+            {
+                MessageBox.Show("Error al ordenar " + errorr);
+                conexion.Close();
+            }   
+        }
+    }
+}
